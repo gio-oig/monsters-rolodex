5f3a2a895b1a2866bfbef6bda60636efbf15d558
giorgi <araxamiag@gmail.com>
2020-07-22 17:02:25 +0400

    Updates

---
+++ precache-manifest.b6caaa419202194d22f9feeef757919e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daadfcd373861cf63ff1d89178dc461a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c109019c10795c91004c",
+    "url": "/monsters-rolodex/static/css/main.027114c9.chunk.css"
+  },
+  {
+    "revision": "eb32b41f166e99422f98",
+    "url": "/monsters-rolodex/static/js/2.ba95ac2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.ba95ac2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c109019c10795c91004c",
+    "url": "/monsters-rolodex/static/js/main.fc9cce82.chunk.js"
+  },
+  {
+    "revision": "cb2809541acad98218f5",
+    "url": "/monsters-rolodex/static/js/runtime-main.71abfee9.js"
+  }
+]);
